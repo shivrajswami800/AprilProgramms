0b27808ec26a1763d0b0aee6c0c6a52be677641a
shivraj <shivrajswami800@gmail.com>
2019-04-06 19:28:50 +0530

    two programm added successfully

---
+++ EnhansedS.java
@@ -0,0 +1,16 @@
+package Kapil.Basic;
+
+public class EnhansedS {
+
+	public static void main(String[] args) 
+	{
+		String subject[]= {"Math","History","Geography","Polity"};
+		for(String Vishay:subject)
+		{
+			System.out.println(Vishay);
+		}
+
+		
+	 }
+
+}
---
+++ InhansedFor.java
@@ -0,0 +1,14 @@
+package Kapil.Basic;
+
+public class InhansedFor {
+
+	public static void main(String[] args) {
+		int a[] = {35,87,65,54,43,21};
+		for(int d:a)
+		{
+			System.out.println(d);
+		}
+
+	}
+
+}
